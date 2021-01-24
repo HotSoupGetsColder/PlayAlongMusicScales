ce6fdddedbae1f13ec853de805711ced3d4fe740
HotSoupGetsColder <mattamath@gmail.com>
2021-01-24 00:38:24 -0500

    add buttons

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,15 +1,17 @@
-let osc, env, midiValue, step, currentOctave, playingScale;
+let osc, env, midiValue, step, currentOctave, playingScale, inpNoteType;
 
 let scaleArray = [57, 59, 61, 62, 64, 66, 68, 69];
 let majorScaleSteps = [2, 2, 1, 2, 2, 2, 1];
+let noteTypes = [1, 2, 4, 8, 16];
 
 let aMStart = 57;
-let octaves = 5;
-let tempo = 400; //in BPM
+let octaves = 3;
+let tempo = 60; //in BPM
+let note = 8; // 8 = eighth, 4 = quarter, etc.
 
 let frameRate = 60;
 let w = 200;
-let h = 200;
+let h = 50;
 
 var button;
 
@@ -21,14 +23,8 @@ function startScale(){
   playingScale = true;
 }
 
-// function nextOctave() {
-//   currentOctave++;
-//   if (currentOctave <= octaves){
-//     step = 0;
-//   }
-// }
-
 function playNote() {
+  env.mult(0.1 * volSlider.value());
   let freqValue = midiToFreq(midiValue);
   osc.freq(freqValue);
   env.play(osc);
@@ -52,6 +48,13 @@ function checkEnd() {
   }
 }
 
+function changeNoteType() {
+  note = inpNoteType.value();
+  let secPerNote = (60 / tempo) * (4 / note);
+  env.setADSR(0.1, 0.1, 0.8, secPerNote);
+  env.setRange(1, 0);
+}
+
 function setup() {
   createCanvas(w, h);
   background(100, 255, 255);
@@ -61,17 +64,25 @@ function setup() {
 
   button = createButton('start');
   button.mousePressed(startScale);
+  volSlider = createSlider(0, 1, 0.5, 0.05);
+
+  inpNoteType = createSelect();
+  inpNoteType.position(0, 90);
+  for (var i = 0; i < noteTypes.length; i++) {
+    inpNoteType.option(noteTypes[i]);
+  }
+  inpNoteType.selected(4);
+  inpNoteType.changed(changeNoteType);
 
   osc = new p5.Oscillator('sine');
   env = new p5.Envelope();
-  let secPerBeat = 60 / tempo;
-  env.setADSR(0.1, 0.1, 0.8, secPerBeat);
-  env.setRange(1, 0);
+
+  changeNoteType();
 }
 
 function draw() {
   // put drawing code here
-  if (frameCount % (60 / tempo * frameRate) === 0 || frameCount === 1) {
+  if (frameCount % ((60 / tempo) * (4 / note) * frameRate) === 0 || frameCount === 1) {
     if (playingScale) {
       playNote();
       updateScale();
