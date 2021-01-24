32af6077dcf2310be3f47addee6538ef6a880b22
HotSoupGetsColder <mattamath@gmail.com>
2021-01-23 19:47:13 -0500

    add octaves and remove repeating top note

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,13 +1,13 @@
-let osc, env, midiValue;
+let osc, env, midiValue, step, currentOctave;
 
 let scaleArray = [57, 59, 61, 62, 64, 66, 68, 69];
 let majorScaleSteps = [2, 2, 1, 2, 2, 2, 1];
 
 let aMStart = 57;
 let octaves = 2;
-let totalIndex = 0;
-let note = 0;
-let tempo = 120; //in BPM
+// let totalIndex = 0;
+// let note = 0;
+let tempo = 300; //in BPM
 
 let frameRate = 60;
 let w = 200;
@@ -28,23 +28,41 @@ function setup() {
   env.setADSR(0.1, 0.1, 0.8, secPerBeat);
   env.setRange(1, 0);
   osc.start();
+
+  midiValue = 57;
+  step = 0;
+  currentOctave = 1;
 }
 
 function draw() {
   // put drawing code here
   if (frameCount % (60 / tempo * frameRate) === 0 || frameCount === 1) {
-    if (totalIndex === 0) {
-      midiValue = aMStart;
-      note = 0;
-    }
-    midiValue = midiValue + majorScaleSteps[note];
     let freqValue = midiToFreq(midiValue);
     osc.freq(freqValue);
+    env.play(osc);
 
-    print(note + ' ' + midiValue + ' ' + freqValue);
+    midiValue = midiValue + majorScaleSteps[step];
+    step++;
+    if (step >= majorScaleSteps.length) {
+      currentOctave++;
+      if (currentOctave <= octaves){
+        step = 0;
+      }
+    }
 
-    env.play(osc, 0, 0.05);
-    note = (note + 1) % majorScaleSteps.length;
-    totalIndex = (totalIndex + 1) % (majorScaleSteps.length * octaves);
+
+    // if (totalIndex === 0) {
+    //   midiValue = aMStart;
+    //   note = 0;
+    // }
+    // midiValue = midiValue + majorScaleSteps[note];
+    // let freqValue = midiToFreq(midiValue);
+    // osc.freq(freqValue);
+    //
+    // print(note + ' ' + midiValue + ' ' + freqValue);
+    //
+    // env.play(osc, 0, 0.05);
+    // note = (note + 1) % majorScaleSteps.length;
+    // totalIndex = (totalIndex + 1) % (majorScaleSteps.length * octaves);
   }
 }
