cbf79890b5742ec0fae0dee6c8cf86082fe83997
HotSoupGetsColder <mattamath@gmail.com>
2021-01-23 19:06:50 -0500

    make basic scale

---
+++ empty-example/sketch.js
@@ -0,0 +1,50 @@
+let osc, env, midiValue;
+
+let scaleArray = [57, 59, 61, 62, 64, 66, 68, 69];
+let majorScaleSteps = [2, 2, 1, 2, 2, 2, 1];
+
+let aMStart = 57;
+let octaves = 2;
+let totalIndex = 0;
+let note = 0;
+let tempo = 120; //in BPM
+
+let frameRate = 60;
+let w = 200;
+let h = 200;
+
+function setup() {
+  createCanvas(w, h);
+  background(100, 255, 255);
+  textSize(20);
+  textAlign(CENTER, CENTER);
+  text('A Major Scale', w/2, h/2);
+
+  let secPerBeat = 60 / tempo;
+
+  osc = new p5.Oscillator('sine');
+
+  env = new p5.Envelope();
+  env.setADSR(0.1, 0.1, 0.8, secPerBeat);
+  env.setRange(1, 0);
+  osc.start();
+}
+
+function draw() {
+  // put drawing code here
+  if (frameCount % (60 / tempo * frameRate) === 0 || frameCount === 1) {
+    if (totalIndex === 0) {
+      midiValue = aMStart;
+      note = 0;
+    }
+    midiValue = midiValue + majorScaleSteps[note];
+    let freqValue = midiToFreq(midiValue);
+    osc.freq(freqValue);
+
+    print(note + ' ' + midiValue + ' ' + freqValue);
+
+    env.play(osc, 0, 0.05);
+    note = (note + 1) % majorScaleSteps.length;
+    totalIndex = (totalIndex + 1) % (majorScaleSteps.length * octaves);
+  }
+}
